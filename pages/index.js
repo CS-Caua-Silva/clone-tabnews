7f7554f80e550a18971b1e323ef6cac1d048faa9
Cauã Rodrigues da Silva <cauardasilva@gmail.com>
2025-12-16 18:37:10 +0000

    adiciona o arquivo .gitignore

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+    return <h1>Amor eu te amoooooooooooooooo
+
+        Linda, gostosa, belissima, maravilhosa, perfeita
+
+        <p>Eu te amo muito minha vida</p> 
+    </h1>
+    
+}
+
+export default Home;
+
